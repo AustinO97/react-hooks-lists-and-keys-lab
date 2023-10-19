742fd2527a11c0795ed4774467f16b54bf53bd54
Austin Oblow <austinoblow97@gmail.com>
2023-10-19 15:50:50 -0500

    completed lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  const projectsComp = projects.map(obj => {
+    return <ProjectItem key={obj.id} name={obj.name} about={obj.about} technologies={obj.technologies}/>
+  })
+  console.log(projectsComp);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsComp}</div>
     </div>
   );
 }
